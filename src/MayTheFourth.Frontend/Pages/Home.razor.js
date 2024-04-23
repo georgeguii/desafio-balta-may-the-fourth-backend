3eadb836bf814179ec69ffbba738389d0d8b9c78
George Guilherme <george.gui2000@gmail.com>
2024-04-22 21:10:20 -0300

    refactor: home.razor with css and js scoped

---
+++ src/MayTheFourth.Frontend/Pages/Home.razor.js
@@ -0,0 +1,33 @@
+﻿const setStyle = (element, top, left) => {
+    element.style.top = top + '%';
+    element.style.left = left + '%';
+};
+
+const appendToBody = (element) => {
+    document.body.appendChild(element);
+};
+
+const createStar = () => {
+    const star = document.createElement("div");
+    star.className = 'stars';
+
+    const delay = Math.random() * 5;
+    star.style.animationDelay = delay + "s";
+
+    return star;
+};
+
+const randomPosition = () => {
+    const randomX = Math.round(Math.random() * 10000) / 100;
+    const randomY = Math.round(Math.random() * 10000) / 100;
+    return [randomX, randomY];
+};
+
+export async function createAndPositionStars(count) {
+    for (let i = 0; i < count; i++) {
+        const star = createStar();
+        const xy = randomPosition();
+        setStyle(star, xy[0], xy[1]);
+        appendToBody(star);
+    }
+};
