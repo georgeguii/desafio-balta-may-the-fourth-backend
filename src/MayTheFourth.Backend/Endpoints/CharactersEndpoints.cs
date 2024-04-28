0773b98ee95957af1d49d1b832f41222b8e230d8
George Guilherme <george.gui2000@gmail.com>
2024-04-27 22:48:48 -0300

    feat: characters endpoints

---
+++ src/MayTheFourth.Backend/Endpoints/CharactersEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using MayTheFourth.Backend.DataBase;
+using MayTheFourth.Backend.Entitites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFourth.Backend.Endpoints;
+
+public static class CharactersEndpoints
+{
+    public static void AddCharactersEndpoints(this RouteGroupBuilder route)
+    {
+        var charactersRoutes = route.MapGroup("characters");
+
+        charactersRoutes.MapGet("/", async ([FromServices] ApiDbContext context) =>
+             await context.Characters.AsNoTracking().ToListAsync())
+        .Produces<List<Character>>(StatusCodes.Status200OK)
+        .WithName("GetAllCharacters")
+        .WithTags("Character");
+
+        charactersRoutes.MapGet("/{id}", async (int id, [FromServices] ApiDbContext context) =>
+             await context.Characters.FirstOrDefaultAsync(f => f.Id == id)
+                is Character character
+                ? Results.Ok(character)
+                : Results.NotFound())
+        .Produces<Character>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound)
+        .WithName("GetCharacterById")
+        .WithTags("Character");
+    }
+}
